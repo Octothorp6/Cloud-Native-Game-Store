525a5764f3b1a8212a45bd5b2b2061c91189df31
SOAMI <sac593@nyu.edu>
2019-10-08 22:22:04 -0400

    createGetGetAll mock passed

--- product-service/src/test/java/com/trilogyed/productservice/service/ServiceLayerTest.java
+++ product-service/src/test/java/com/trilogyed/productservice/service/ServiceLayerTest.java
@@ -4,13 +4,14 @@ import com.trilogyed.productservice.dao.ProductDao;
 import com.trilogyed.productservice.dao.ProductDaoJdbcTempImpl;
 import com.trilogyed.productservice.model.Product;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class ServiceLayerTest {
     ProductDao dao;
@@ -51,7 +52,7 @@ public class ServiceLayerTest {
         productB.setUnitCost(new BigDecimal("550.99"));
 
         doReturn(productA).when(dao).createProduct(productB);
-        doReturn(productA).when(dao).getProduct(1);
+        doReturn(productA).when(dao).getProduct(productA.getProductId());
         //====================================================
 
         Product productC = new Product();
@@ -68,7 +69,7 @@ public class ServiceLayerTest {
         productD.setUnitCost(new BigDecimal("50.99"));
 
         doReturn(productC).when(dao).createProduct(productD);
-        doReturn(productC).when(dao).getProduct(2);
+        doReturn(productC).when(dao).getProduct(productC.getProductId());
         //===================================================
         //Product List, size 2
 
@@ -96,14 +97,64 @@ public class ServiceLayerTest {
         productNotUpdatedId.setUnitCost(new BigDecimal("2399.99"));
 
         Product productUpdated = new Product();
+        productUpdated.setProductId(22);
         productUpdated.setName("Razer Blade Laptop 14' ");
         productUpdated.setDescription("Razer's 14 inch gaming laptop");
         productUpdated.setListPrice(new BigDecimal("2299.99")); //Sale of $200
         productUpdated.setUnitCost(new BigDecimal("2099.99"));
 
+        doReturn(productNotUpdatedId).when(dao).createProduct(productNoId);
+        doNothing().when(dao).updateProduct(productUpdated);
+        doReturn(productUpdated).when(dao).getProduct(productUpdated.getProductId());
 
+        //===================================================
+        //DELETE
 
+        Product productDelete = new Product();
+        productDelete.setProductId(99);
+        productDelete.setName("Macbook Pro 15' ");
+        productDelete.setDescription("15 inch macbook pro");
+        productDelete.setListPrice(new BigDecimal("2100.99"));
+        productDelete.setUnitCost(new BigDecimal("1999.99"));
 
+        doNothing().when(dao).deleteProduct(productDelete.getProductId());
+        doReturn(null).when(dao).getProduct(productDelete.getProductId());
+    }
+    /*
+        public Product createProduct(Product product)
+
+    public void updateProduct(Product product)
+
+    public Product getProduct(int productId)
+
+    public List<Product> getAllProducts()
+
+    public void deleteProduct(int productId)
+     */
+
+    @Test
+    public void createGetGetAllProduct(){
+        Product product = new Product();
+        product.setName("Xbox Scorpion");
+        product.setDescription("Microsoft's latest console");
+        product.setListPrice(new BigDecimal("599.99"));
+        product.setUnitCost(new BigDecimal("550.99"));
+
+        product = service.createProduct(product);
+
+        Product productFromService = service.getProduct(product.getProductId());
+        assertEquals(product,productFromService);
+
+        Product product2 = new Product();
+        product2.setName("Call of Duty: Modern Warfare");
+        product2.setDescription("Activision's newest addition, coming oct 22");
+        product2.setListPrice(new BigDecimal("59.99"));
+        product2.setUnitCost(new BigDecimal("50.99"));
+
+        product2 = service.createProduct(product2);
+
+        List<Product> productList = service.getAllProducts();
+        assertEquals(2,productList.size());
 
 
     }
