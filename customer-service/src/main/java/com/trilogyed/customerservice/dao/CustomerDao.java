d60ecf2156e3f0d9f5fc56ac5937c5e087da8efe
SOAMI <sac593@nyu.edu>
2019-10-08 23:48:46 -0400

    finished DAO and exception class

--- customer-service/src/main/java/com/trilogyed/customerservice/dao/CustomerDao.java
+++ customer-service/src/main/java/com/trilogyed/customerservice/dao/CustomerDao.java
@@ -1,5 +1,15 @@
 package com.trilogyed.customerservice.dao;
 
+import com.trilogyed.customerservice.model.Customer;
+
+import java.util.List;
+
 public interface CustomerDao {
+
+    Customer createCustomer(Customer customer);
+    Customer getCustomer(int customerId);
+    List<Customer> getAllCustomers();
+    void updateCustomer(Customer customer);
+    void deleteCustomer(int customerId);
 
 }
