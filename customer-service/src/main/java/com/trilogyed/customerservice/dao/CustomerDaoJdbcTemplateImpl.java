bc6d0695e16e6b4b28eed0a0c1b2c7c18f047c17
SOAMI <sac593@nyu.edu>
2019-10-08 23:49:35 -0400

    starting red-green refactor for TDD for Customer

--- customer-service/src/main/java/com/trilogyed/customerservice/dao/CustomerDaoJdbcTemplateImpl.java
+++ customer-service/src/main/java/com/trilogyed/customerservice/dao/CustomerDaoJdbcTemplateImpl.java
@@ -1,4 +1,32 @@
 package com.trilogyed.customerservice.dao;
 
-public class CustomerDaoJdbcTemplateImpl {
+import com.trilogyed.customerservice.model.Customer;
+
+import java.util.List;
+
+public class CustomerDaoJdbcTemplateImpl implements CustomerDao {
+    @Override
+    public Customer createCustomer(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public Customer getCustomer(int customerId) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return null;
+    }
+
+    @Override
+    public void updateCustomer(Customer customer) {
+
+    }
+
+    @Override
+    public void deleteCustomer(int customerId) {
+
+    }
 }
