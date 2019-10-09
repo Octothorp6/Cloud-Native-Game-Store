e65265c404d0d8a31a2f4c9b728a229cb13b1e87
SOAMI <sac593@nyu.edu>
2019-10-08 23:45:27 -0400

    customer packages, skeleton classes added, model class FINISHED

---
+++ customer-service/src/main/java/com/trilogyed/customerservice/exception/NotFoundException.java
@@ -0,0 +1,4 @@
+package com.trilogyed.customerservice.exception;
+
+public class NotFoundException {
+}
---
+++ customer-service/src/test/java/com/trilogyed/customerservice/dao/CustomerDaoJdbcTemplateImplTest.java
@@ -0,0 +1,4 @@
+package com.trilogyed.customerservice.dao;
+
+public class CustomerDaoJdbcTemplateImplTest {
+}
