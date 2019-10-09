d1bc9a03b9a6aea0c7ab5243f9b6d45506b9adba
SOAMI <sac593@nyu.edu>
2019-10-09 01:14:27 -0400

    post for customer controller done

--- customer-service/src/main/java/com/trilogyed/customerservice/controller/CustomerController.java
+++ customer-service/src/main/java/com/trilogyed/customerservice/controller/CustomerController.java
@@ -1,4 +1,24 @@
 package com.trilogyed.customerservice.controller;
 
+import com.trilogyed.customerservice.model.Customer;
+import com.trilogyed.customerservice.service.ServiceLayer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+//@RefreshScope
+@RequestMapping("/customers")
 public class CustomerController {
+
+    @Autowired
+    ServiceLayer service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Customer createCustomer(@RequestBody @Valid Customer customer){return service.createCustomer(customer);}
+
+    
 }
