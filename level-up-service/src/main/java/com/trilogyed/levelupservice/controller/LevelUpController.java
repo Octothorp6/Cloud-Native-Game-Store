23797ad52d2e56050650f79f65f113b5d6b10602
Blora616 <Blora616@gmail.com>
2019-10-08 22:00:55 -0400

    init levelup

---
+++ level-up-service/src/main/java/com/trilogyed/levelupservice/controller/LevelUpController.java
@@ -0,0 +1,10 @@
+package com.trilogyed.levelupservice.controller;
+
+import com.trilogyed.levelupservice.model.LevelUp;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LevelUpController {
+
+
+}
