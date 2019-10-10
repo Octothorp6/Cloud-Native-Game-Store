f6b137c0b96564289e7543fbb9831e48bc4eb785
SOAMI <sac593@nyu.edu>
2019-10-10 15:15:44 -0400

    added customer model to admin api

--- customer-service/src/main/java/com/trilogyed/customerservice/model/Customer.java
+++ customer-service/src/main/java/com/trilogyed/customerservice/model/Customer.java
@@ -6,16 +6,7 @@ import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public class Customer {
-    /*
-    customer_id int(11) not null auto_increment primary key,
-    first_name varchar(50) not null,
-    last_name varchar(50) not null,
-    street varchar(50) not null,
-    city varchar(50) not null,
-    zip varchar(10) not null,
-    email varchar(75) not null,
-    phone varchar(20) not null
-     */
+
     @NotNull
     private int customerId;
 
