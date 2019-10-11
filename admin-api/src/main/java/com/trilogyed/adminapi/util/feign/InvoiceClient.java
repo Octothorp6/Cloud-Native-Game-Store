15979ea8059ebecc847e86ffc8a75c53716ac8be
SOAMI <sac593@nyu.edu>
2019-10-10 22:11:27 -0400

    made changes to the invoice and invoiceItem feign clients in admin api

--- admin-api/src/main/java/com/trilogyed/adminapi/util/feign/InvoiceClient.java
+++ admin-api/src/main/java/com/trilogyed/adminapi/util/feign/InvoiceClient.java
@@ -2,6 +2,7 @@ package com.trilogyed.adminapi.util.feign;
 
 import com.sun.corba.se.pept.transport.InboundConnectionCache;
 import com.trilogyed.adminapi.model.Invoice;
+import com.trilogyed.adminapi.model.InvoiceItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
@@ -10,7 +11,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @FeignClient(name = "invoice-service")
-@RequestMapping("/invoice")
+@RequestMapping("/invoices")
 public interface InvoiceClient {
 
     @PostMapping
@@ -27,5 +28,22 @@ public interface InvoiceClient {
 
     @DeleteMapping(value = "/{id}")
     public void deleteInvoice(@PathVariable int id);
+
+    //==============================    InvocieItem Crud ================================
+
+    @PostMapping(value = "/invoiceItems")
+    public InvoiceItem createInvoiceItem(@RequestBody @Valid InvoiceItem invoiceItem);
+
+    @GetMapping(value = "/invoiceItems/{id}")
+    public InvoiceItem getInvoiceItem(@PathVariable int id);
+
+    @GetMapping(value = "/invoiceItems")
+    public List<InvoiceItem> getAllInvoiceItems();
+
+    @PutMapping(value = "/invoiceItem")
+    public void updateInvoiceItem(@RequestBody @Valid InvoiceItem invoiceItem);
+
+    @DeleteMapping(value = "/invoiceItem/{id}")
+    public void deleteInvoiceItem(@PathVariable int id);
 
 }
