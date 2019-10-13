dcef302b4d8c89512faf85ee539c2cd85bffa29f
SOAMI <sac593@nyu.edu>
2019-10-12 22:32:31 -0400

    set up both levelup and product mocks, coding mock tests now

--- admin-api/src/test/java/com/trilogyed/adminapi/service/AdminServiceLayerTest.java
+++ admin-api/src/test/java/com/trilogyed/adminapi/service/AdminServiceLayerTest.java
@@ -1,9 +1,6 @@
 package com.trilogyed.adminapi.service;
 
-import com.trilogyed.adminapi.model.Customer;
-import com.trilogyed.adminapi.model.Inventory;
-import com.trilogyed.adminapi.model.Invoice;
-import com.trilogyed.adminapi.model.InvoiceItem;
+import com.trilogyed.adminapi.model.*;
 import com.trilogyed.adminapi.service.AdminService;
 import com.trilogyed.adminapi.util.feign.*;
 import org.junit.Before;
@@ -233,11 +230,113 @@ public class AdminServiceLayerTest {
     }
 
     private void levelUpMockSetUp(){
+        levelUpClient = mock(LevelUpClient.class);
+
+        LevelUp levelUp = new LevelUp();
+        levelUp.setLevelUpId(1);
+        levelUp.setCustomerId(5);
+        levelUp.setPoints(100);
+        levelUp.setMemberDate(LocalDate.of(2019,11,10));
+
+        LevelUp levelUp1 = new LevelUp();
+        levelUp1.setCustomerId(5);
+        levelUp1.setPoints(100);
+        levelUp1.setMemberDate(LocalDate.of(2019,11,10));
+
+        List<LevelUp> levelUps = new ArrayList<>();
+        levelUps.add(levelUp);
+
+        doReturn(levelUp).when(levelUpClient).createLevelUp(levelUp1);
+        doReturn(levelUp).when(levelUpClient).getLevelUp(1);
+        doReturn(levelUp).when(levelUpClient).getLevelUpByCustomer(5);
+        doReturn(levelUps).when(levelUpClient).getLevelUps();
 
     }
 
     private void productMockSetUp(){
+        productClient = mock(ProductClient.class);
 
+        Product productA = new Product();
+        productA.setProductId(1);
+        productA.setName("Xbox Scorpion");
+        productA.setDescription("Microsoft's latest console");
+        productA.setListPrice(new BigDecimal("599.99"));
+        productA.setUnitCost(new BigDecimal("550.99"));
+
+        Product productB = new Product();
+        productB.setName("Xbox Scorpion");
+        productB.setDescription("Microsoft's latest console");
+        productB.setListPrice(new BigDecimal("599.99"));
+        productB.setUnitCost(new BigDecimal("550.99"));
+
+        doReturn(productA).when(productClient).createProduct(productB);
+        doReturn(productA).when(productClient).getProduct(productA.getProductId());
+        //====================================================
+
+        Product productC = new Product();
+        productC.setProductId(2);
+        productC.setName("Call of Duty: Modern Warfare");
+        productC.setDescription("Activision's newest addition, coming oct 22");
+        productC.setListPrice(new BigDecimal("59.99"));
+        productC.setUnitCost(new BigDecimal("50.99"));
+
+        Product productD = new Product();
+        productD.setName("Call of Duty: Modern Warfare");
+        productD.setDescription("Activision's newest addition, coming oct 22");
+        productD.setListPrice(new BigDecimal("59.99"));
+        productD.setUnitCost(new BigDecimal("50.99"));
+
+        doReturn(productC).when(productClient).createProduct(productD);
+        doReturn(productC).when(productClient).getProduct(productC.getProductId());
+        //===================================================
+        //Product List, size 2
+
+        List<Product> productList = new ArrayList<>();
+        productList.add(productA);
+        productList.add(productC);
+
+        doReturn(productList).when(productClient).getAllProducts();
+
+
+        //===================================================
+        //UPDATE
+
+        Product productNoId = new Product();
+        productNoId.setName("Razer Blade Laptop 14' ");
+        productNoId.setDescription("Razer's 14 inch gaming laptop");
+        productNoId.setListPrice(new BigDecimal("2499.99"));
+        productNoId.setUnitCost(new BigDecimal("2399.99"));
+
+        Product productNotUpdatedId = new Product();
+        productNotUpdatedId.setProductId(22);
+        productNotUpdatedId.setName("Razer Blade Laptop 14' ");
+        productNotUpdatedId.setDescription("Razer's 14 inch gaming laptop");
+        productNotUpdatedId.setListPrice(new BigDecimal("2499.99"));
+        productNotUpdatedId.setUnitCost(new BigDecimal("2399.99"));
+
+        Product productUpdated = new Product();
+        productUpdated.setProductId(22);
+        productUpdated.setName("Razer Blade Laptop 14' ");
+        productUpdated.setDescription("Razer's 14 inch gaming laptop");
+        productUpdated.setListPrice(new BigDecimal("2299.99")); //Sale of $200
+        productUpdated.setUnitCost(new BigDecimal("2099.99"));
+
+        doReturn(productNotUpdatedId).when(productClient).createProduct(productNoId);
+        doNothing().when(productClient).updateProduct(productUpdated);
+        doReturn(productUpdated).when(productClient).getProduct(productUpdated.getProductId());
+
+        //===================================================
+        //DELETE
+
+        Product productDelete = new Product();
+        productDelete.setProductId(99);
+        productDelete.setName("Macbook Pro 15' ");
+        productDelete.setDescription("15 inch macbook pro");
+        productDelete.setListPrice(new BigDecimal("2100.99"));
+        productDelete.setUnitCost(new BigDecimal("1999.99"));
+
+        doNothing().when(productClient).deleteProduct(productDelete.getProductId());
+        doReturn(null).when(productClient).getProduct(productDelete.getProductId());
     }
 
 
