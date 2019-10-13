92408cd0155d3ffac8cc49f44d6129d4ef529296
Blora616 <Blora616@gmail.com>
2019-10-13 01:03:06 -0400

    n

--- retail-api/src/main/java/com/trilogyed/retailapi/service/ServiceLayer.java
+++ retail-api/src/main/java/com/trilogyed/retailapi/service/ServiceLayer.java
@@ -1,10 +1,13 @@
 package com.trilogyed.retailapi.service;
 
-import com.trilogyed.retailapi.model.LevelUp;
+import com.trilogyed.retailapi.model.*;
 import com.trilogyed.retailapi.util.feign.*;
 import com.trilogyed.retailapi.viewmodel.InvoiceViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 /**
  * RETAIL SERVICE LAYER
@@ -34,31 +37,80 @@ public class ServiceLayer {
 
     //==============================================================================================================
     // CUSTOMER METHODS
+    public Customer findCustomerById(int customerId) {
+        return null;
+    }
 
+    public Customer saveCustomer() {
+        return null;
+    }
 
-
+    public List<Customer> findAllCustomers() {
+        return null;
+    }
 
     //==============================================================================================================
     // INVENTORY METHODS
+    public Inventory findInventory(int inventoryId) {
+        return null;
+    }
 
+    public Inventory findInventoryByProductId(int productId) {
+        return null;
+    }
 
+    public List<Inventory> findAllInventory() {
+        return null;
+    }
 
-
+    public Inventory saveInventory(Inventory inventory) {
+        return null;
+    }
     //==============================================================================================================
     // INVOICE METHODS
+    public InvoiceViewModel findInvoiceById(int invoiceId) {
+        return null;
+    }
 
+    public List<InvoiceViewModel> findInvoicesByCustomerId(int customerId) {
+        return null;
+    }
 
+    public List<InvoiceViewModel> findAllInvoices() {
+        return null;
+    }
 
+    public InvoiceViewModel saveInvoice(Invoice invoice) {
+        return null;
+    }
 
     //==============================================================================================================
     // LEVEL-UP METHODS
+    public LevelUp findLevelUp(int levelUpId) {
+        return null;
+    }
 
+    public LevelUp findLevelUpByCustomer(int customerId) {
+        return null;
+    }
 
-
+    public List<LevelUp> findAllLevelUps() {
+        return null;
+    }
 
     //==============================================================================================================
     // PRODUCT METHODS
+    public Product findProduct(int productId) {
+        return null;
+    }
 
+    public Product findProductByProductName(String productName) {
+        return null;
+    }
+
+    public List<Product> findAllProducts() {
+        return null;
+    }
 
 
     // FALL-BACK METHOD
