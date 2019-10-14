f2e027217e52445975d48bbcaa4240056a324714
Blora616 <Blora616@gmail.com>
2019-10-14 11:32:24 -0400

    i

--- retail-api/src/main/java/com/trilogyed/retailapi/util/feign/CustomerClient.java
+++ retail-api/src/main/java/com/trilogyed/retailapi/util/feign/CustomerClient.java
@@ -5,16 +5,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @FeignClient(name = "customer-service")
 @RequestMapping(value = "/customers")
 public interface CustomerClient {
     @PostMapping
     Customer createCustomer(@RequestBody @Valid Customer customer);
-
-    @GetMapping
-    List<Customer> getAllCustomers();
 
     @GetMapping(value = "/{customerId}")
     Customer getCustomer(@PathVariable int customerId);
