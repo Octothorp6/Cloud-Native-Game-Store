62a67d303ea1d5d640b9409af2f0f00f2df19504
SOAMI <sac593@nyu.edu>
2019-10-13 23:42:23 -0400

    security commented out in invoice controller

--- admin-api/src/main/java/com/trilogyed/adminapi/controller/InvoiceController.java
+++ admin-api/src/main/java/com/trilogyed/adminapi/controller/InvoiceController.java
@@ -90,38 +90,51 @@ public class InvoiceController {
 //    @DeleteMapping(value = "/invoice-items/{id}")
 //    public void deleteInventory(@PathVariable int id){adminService.deleteInvoiceItem(id);}
 
-
+//------------------------------------------------------------------------------------------------------------------------
     //Security impl below
     //==========================================    Invoice    ========================================================
 
-//    @PostMapping
-//    public Invoice createInvoice(Principal principal, @RequestBody @Valid Invoice invoice){return adminService.createInvoice(invoice);}
+//    @RequestMapping(value = "/admin/invoices",method = RequestMethod.POST)
+//    @ResponseStatus(value = HttpStatus.CREATED)
+//    public InvoiceViewModel createInvoice(Principal principal,@RequestBody @Valid Invoice invoice){return adminService.createInvoice(invoice);}
 //
-//    @GetMapping(value = "/{id}")
-//    public Invoice getInvoice(@PathVariable int id){
-//        Invoice invoiceFromService = adminService.getInvoice(id);
+//    @RequestMapping(value = "/admin/invoices/{id}",method = RequestMethod.GET)
+//    @ResponseStatus(value = HttpStatus.OK)
+//    public InvoiceViewModel getInvoice(@PathVariable int id){
+//        InvoiceViewModel invoiceFromService = adminService.getInvoice(id);
 //        if(invoiceFromService == null)
 //            throw new NotFoundException("No Invoice exists in the DB with given id: "+id);
 //        return invoiceFromService;
 //    }
 //
-//    @GetMapping
-//    public List<Invoice> getAllInvoices(){return adminService.getAllInvoices();}
+//    @RequestMapping(value = "/admin/invoices/all", method = RequestMethod.GET)
+//    @ResponseStatus(value = HttpStatus.OK)
+//    public List<InvoiceViewModel> getAllInvoices(){return adminService.getAllInvoices();}
 //
-//    @PutMapping
-//    public void updateInvoice(Principal principal,@RequestBody @Valid Invoice invoice){adminService.updateInvoice(invoice);}
+//    @RequestMapping(value = "/admin/invoices/customer/{id}",method = RequestMethod.GET)
+//    @ResponseStatus(value = HttpStatus.OK)
+//    public List<InvoiceViewModel> getAllInvoicesByCustomer(@PathVariable int id){
+//        return adminService.getAllInvoicesByCustomer(id);
+//    }
 //
+//    @RequestMapping(value = "/admin/invoice", method = RequestMethod.PUT)
+//    @ResponseStatus(HttpStatus.NO_CONTENT)
+//    public void updateInvoice(Principal principal, @RequestBody @Valid Invoice invoice){adminService.updateInvoice(invoice);}
+
 //    @DeleteMapping(value = "/{id}")
-//    public  void deleteInvoice(Principal principal,@PathVariable int id){adminService.deleteInvoice(id);}
+//    public  void deleteInvoice(@PathVariable int id){adminService.deleteInvoice(id);}
+
+    //==========================================    InvoiceItem    ====================================================
 //
-//    //==========================================    InvoiceItem    ====================================================
-//
-//    @PostMapping(value = "/invoice-items")
+//    @RequestMapping(value = "/admin/invoice-items",method = RequestMethod.POST)
+//    @ResponseStatus(value = HttpStatus.CREATED)
 //    public InvoiceItem createInvoiceItem(Principal principal,@RequestBody @Valid InvoiceItem invoiceItem){
 //        return adminService.createInvoiceItem(invoiceItem);
 //    }
 //
-//    @GetMapping(value = "/invoice-items/{id}")
+//
+//    @RequestMapping(value = "/admin/invoice-items/{id}",method = RequestMethod.GET)
+//    @ResponseStatus(value = HttpStatus.OK)
 //    public InvoiceItem getInvoiceItem(@PathVariable int id){
 //        InvoiceItem invItemFromService = adminService.getInvoiceItem(id);
 //        if(invItemFromService == null )
@@ -129,15 +142,16 @@ public class InvoiceController {
 //        return invItemFromService;
 //    }
 //
-//    @GetMapping(value = "/invoice-items")
+//
+//    @RequestMapping(value = "/admin/invoice-items/all", method = RequestMethod.GET)
+//    @ResponseStatus(value = HttpStatus.OK)
 //    public List<InvoiceItem> getAllInvoiceItems(){return adminService.getAllInvoiceItems();}
 //
-//    @PutMapping(value = "/invoice-items")
+//
+//    @RequestMapping(value = "/invoice-items", method = RequestMethod.PUT)
+//    @ResponseStatus(HttpStatus.NO_CONTENT)
 //    public void updateInvoiceItem(Principal principal,@RequestBody @Valid InvoiceItem invoiceItem){
 //        adminService.updateInvoiceItem(invoiceItem);
 //    }
-//
-//    @DeleteMapping(value = "/invoice-items/{id}")
-//    public void deleteInventory(Principal principal,@PathVariable int id){adminService.deleteInvoiceItem(id);}
 
 }
